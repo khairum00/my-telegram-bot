2d3bf11f236f665e59dd36a7ef77d5f6769d2533
khairum00 <siam162536@gmail.com>
2026-03-15 04:22:29 +0600

    Refactor server.py to improve bot startup process

--- server.py
+++ server.py
@@ -1,20 +1,16 @@
-import os
-import threading
 from flask import Flask
+import threading
+import os
 import bot
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Telegram Bot Running"
+    return "Bot is running"
 
-def start_bot():
-    bot.bot.infinity_polling()
-
-# bot thread start
-threading.Thread(target=start_bot).start()
-
-if __name__ == "__main__":
+def run():
     port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run).start()
